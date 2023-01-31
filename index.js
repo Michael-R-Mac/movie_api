c9a4a61e9353d42f6df65186a769e52f70849e12
Michael <89353459+Michael-R-Mac@users.noreply.github.com>
2023-01-31 11:22:24 +0100

    Introduce Mongoose

--- index.js
+++ index.js
@@ -2,167 +2,187 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const uuid = require('uuid');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 
-app.use(morgan('common'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
-let users = [
-  {
-    id: 1,
-    name: 'John',
-    favoritemovies: [],
-  },
-  {
-    id: 2,
-    name: 'Adam',
-    favoritemovies: ['Interstellar'],
-  },
-];
+app.use(morgan('common'));
 
-let topMovies = [
-  {
-    'Title': 'Forrest Gump',
-    'Description': 'The presidencies of Kennedy and Johnson, the Vietnam War the Watergate scandal and other historical events unfold from the perspective of an Alabama man with an IQ of 75, whose only desire is to be reunited with his childhood sweetheart.',
-    'Genre': {
-      'Name': 'Drama',
-      'Description': 'In film and television, drama is a category or genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.'
-    },
-    'Director': {
-      'Name': 'Robert Zemeckis',
-      'Bio': "A whiz-kid with special effects, Robert is from the Spielberg camp of film-making (Steven Spielberg produced many of his films). Usually working with writing partner Bob Gale, Robert's earlier films show he has a talent for zany comedy (Romancing the Stone (1984), 1941 (1979)) and special effect vehicles (Who Framed Roger Rabbit (1988) and Back to the Future (1985)). His later films have become more serious, with the hugely successful Tom Hanks vehicle Forrest Gump (1994) and the Jodie Foster film Contact (1997), both critically acclaimed movies. Again, these films incorporate stunning effects. Robert has proved he can work a serious story around great effects.",
-      'Birth':1952.0 
-  },
-  'ImageURL': 'https://www.imdb.com/title/tt0109830/mediaviewer/rm1954748672/?ref_=tt_ov_i',
-  'Featured': false
-},
-  {
-    'Title': 'Interstellar',
-    'Description': "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
-    'Genre': {
-      'Name': 'Sci-Fi',
-      'Description': 'Science fiction (or sci-fi) is a film genre that uses speculative, fictional science-based depictions of phenomena that are not fully accepted by mainstream science, such as extraterrestrial lifeforms, spacecraft, robots, cyborgs, interstellar travel, time travel, or other technologies.'
-    },
-    'Director': {
-      'Name': 'Christopher Nolan',
-      'Bio': "Best known for his cerebral, often nonlinear, storytelling, acclaimed writer-director Christopher Nolan was born on July 30, 1970, in London, England. Over the course of 15 years of filmmaking, Nolan has gone from low-budget independent films to working on some of the biggest blockbusters ever made.",
-      'Birth':1970.0 
-  },
-  'ImageURL': 'https://www.imdb.com/title/tt0816692/mediaviewer/rm4043724800/?ref_=tt_ov_i',
-  'Featured': false
-},
-  {
-    'Title': 'The Shawshank Redemption',
-    'Description': "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
-    'Genre': {
-      'Name': 'Drama',
-      'Description': 'In film and television, drama is a category or genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.'
-    },
-    'Director': {
-      'Name': 'Frank Darabont',
-      'Bio': "Three-time Oscar nominee Frank Darabont was born in a refugee camp in 1959 in Montbeliard, France, the son of Hungarian parents who had fled Budapest during the failed 1956 Hungarian revolution. Darabont is one of only six filmmakers in history with the unique distinction of having his first two feature films receive nominations for the Best Picture Academy Award: 1994's The Shawshank Redemption (1994) (with a total of seven nominations) and 1999's The Green Mile (1999) (four nominations). Darabont himself collected Oscar nominations for Best Adapted Screenplay for each film (both based on works by Stephen King), as well as nominations for both films from the Director's Guild of America, and a nomination from the Writers Guild of America for The Shawshank Redemption (1994). He won the Humanitas Prize, the PEN Center USA West Award, and the Scriptor Award for his screenplay of 'The Shawshank Redemption'. For 'The Green Mile', he won the Broadcast Film Critics prize for his screenplay adaptation, and two People's Choice Awards in the Best Dramatic Film and Best Picture categories. The Majestic (2001), starring Jim Carrey, was released in December 2001. He executive-produced the thriller, Collateral (2004), for DreamWorks, with Michael Mann directing and Tom Cruise starring. Future produced-by projects include 'Way of the Rat' at DreamWorks with Chuck Russell adapting and directing the CrossGen comic book series and 'Back Roads', a Tawni O'Dell novel, also at DreamWorks, with Todd Field attached to direct. Darabont and his production company, 'Darkwoods Productions', have an overall deal with Paramount Pictures.",
-      'Birth':1959.0 
-  },
-  'ImageURL': 'https://www.imdb.com/title/tt0111161/mediaviewer/rm10105600/?ref_=tt_ov_i',
-  'Featured': true
-},
-];
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://127.0.0.1:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Create: Post requests
+// Add a user
 app.post('/users', (req, res) => {
-  const newUser = req.body;
-  if (newUser.name) {
-    newUser.id = uuid.v4();
-    users.push(newUser);
-    res.status(201).json(newUser);
-  } else {
-    res.status(400).send('New user requires name');
-  }
+  Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(`${req.body.Username} already exists`);
+      }
+      Users
+        .create({
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday,
+        })
+        .then((user) => { res.status(201).json(user); })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send(`Error: ${error}`);
+        });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send(`Error: ${error}`);
+    });
 });
 
-app.post('/users/:id/:movieTitle', (req, res) => {
-  const { id, movieTitle } = req.params;
-  let user = users.find(user => user.id == id);
-  if (user) {
-    user.favoritemovies.push(movieTitle);
-    res.status(200).send(`${movieTitle} has been added to user ${id} array`);
-  } else {
-    res.status(400).send('User not found');
-  }
+// Add a user favoritemovie
+app.post('/users/:Username/movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    { $push: { FavoriteMovies: req.params.MovieID } },
+    { new: true },
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send(`Error: ${err}`);
+      } else {
+        res.status(200).json(updatedUser);
+      }
+    },
+  );
 });
 
 // Update: Put requests
-app.put('/users/:id', (req, res) => {
-  const { id } = req.params;
-  const updateUser = req.body;
-
-  let user = users.find(user => user.id == id);
-  if (user) {
-    user.name = updateUser.name;
-    res.status(200).json(user);
-  } else {
-    res.status(400).send('User not found');
-  }
+app.put('/users/:Username', (req, res) => {
+  Users.findOneAndUpdate(
+    { Username: req.body.Username },
+    {
+      $set: {
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday,
+      },
+    },
+    { new: true },
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send(`Error: ${err}`);
+      } else {
+        res.json(updatedUser);
+      }
+    },
+  );
 });
 
 // Delete: Delete requests
-app.delete('/users/:id/:movieTitle', (req, res) => {
-  const { id, movieTitle } = req.params;
-  let user = users.find(user => user.id == id);
-  if (user) {
-    user.favoritemovies = user.favoritemovies.filter(title => title !== movieTitle) ;
-    res.status(200).send(`${movieTitle} has been removed from user ${id} array`);
-  } else {
-    res.status(400).send('User not found');
-  }
+// Delete favoritemovie from username
+app.delete('/users/:Username/movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate(
+    { Username: req.body.Username },
+    { $pull: { FavoriteMovies: req.body.MovieID } },
+    { new: true },
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send(`Error: ${err}`);
+      } else {
+        res.status(200).json(updatedUser);
+      }
+    },
+  );
 });
 
-app.delete('/users/:id', (req, res) => {
-  const { id } = req.params;
-  let user = users.find(user => user.id == id);
-  if (user) {
-    users = users.filter(user => user.id != id);
-    res.status(200).send(`User ${id} has been deleted`);
-  } else {
-    res.status(400).send('User not found');
-  }
+// Delete user
+app.delete('/users/:Username', (req, res) => {
+  Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send('User was not found');
+      } else {
+        res.status(200).send('User has been deleted');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(`Error: ${err}`);
+    });
 });
 
 // Read: GET requests
+// Get all users
+app.get('/users', (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(`Error: ${err}`);
+    });
+});
+
+// Get all movies
 app.get('/movies', (req, res) => {
-  res.status(200).json(topMovies);
+  Movies.find()
+    .then((movies) => {
+      res.status(200).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(`Error: ${err}`);
+    });
 });
 
-app.get('/movies/:title', (req, res) => {
-  const { title } = req.params;
-  const movie = topMovies.find(movie => movie.Title === title);
-  if (movie) {
-    res.status(200).json(movie);
-  } else {
-    res.status(400).send('Movie not found');
-  }
+// Get a movie by title
+app.get('/movies/:Title', (req, res) => {
+  Movies.findOne({ Title: req.params.Title })
+    .then((movie) => {
+      res.status(200).json(movie);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(`Error: ${err}`);
+    });
 });
 
-app.get('/movies/genre/:genreName', (req, res) => {
-  const { genreName } = req.params;
-  const genre = topMovies.find( movie => movie.Genre.Name === genreName).Genre;
-  if (genre) {
-    res.status(200).json(genre);
-  } else {
-    res.status(400).send('Genre not found');
-  }
+// Get a movie by genre
+app.get('/movies/genre/:Name', (req, res) => {
+  Movies.findOne({ 'Genre.Name': req.params.Name })
+    .then((Movie) => {
+      res.status(200).json(Movie.Genre.Description);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(`Error: ${err}`);
+    });
 });
 
-app.get('/movies/directors/:directorName', (req, res) => {
-  const { directorName } = req.params;
-  const director = topMovies.find( movie => movie.Director.Name === directorName).Director;
-  if (director) {
-    res.status(200).json(director);
-  } else {
-    res.status(400).send('Director not found');
-  }
+// Get a movie by director
+app.get('/directors/:Name', (req, res) => {
+  Movies.findOne({ 'Director.Name': req.params.Name })
+    .then((Movie) => {
+      res.status(200).json(Movie.Director);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(`Error: ${err}`);
+    });
 });
 
 app.use(express.static('public'));
