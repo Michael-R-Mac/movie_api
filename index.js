ee78d0ba1fc5258a23b75636d456f3684d0d595d
Michael <89353459+Michael-R-Mac@users.noreply.github.com>
2023-02-21 11:09:12 +0100

    Warning suppressor

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-//mongoose.set('strictQuery', true);
+mongoose.set('strictQuery', false);
 //mongoose.connect('mongodb://127.0.0.1:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
 
